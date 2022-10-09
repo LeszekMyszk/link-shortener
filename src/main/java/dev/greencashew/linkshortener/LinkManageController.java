6f02df3cc375a56514772560e3965137ec45392e
Leszek M <Leszek@users.noreply.github.com>
2022-10-09 09:46:54 +0200

    Create link endpoints, hided email in response

---
+++ src/main/java/dev/greencashew/linkshortener/LinkManageController.java
@@ -0,0 +1,15 @@
+package dev.greencashew.linkshortener;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/links")
+class LinkManageController {
+    @PostMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    LinkDto createLink(@RequestBody CreateLinkDto link){
+        return link.toDto();
+    }
+}
