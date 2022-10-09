f964336ca973d1990d49ed9a1efb73ea4199174e
Leszek M <Leszek@users.noreply.github.com>
2022-10-09 16:12:17 +0200

    Add interface and lombok

--- src/main/java/dev/greencashew/linkshortener/controller/LinkManageController.java
+++ src/main/java/dev/greencashew/linkshortener/controller/LinkManageController.java
@@ -2,17 +2,20 @@ package dev.greencashew.linkshortener.controller;
 
 import dev.greencashew.linkshortener.dto.LinkDto;
 import dev.greencashew.linkshortener.link.LinkService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/links")
+
 class LinkManageController {
     private final LinkService linkService;
 
-    LinkManageController(final LinkService linkService) {
-        this.linkService = linkService;
-    }
+   // LinkManageController(final LinkService linkService) {
+  //      this.linkService = linkService;
+ //   }
 
     @PostMapping
     @ResponseBody
